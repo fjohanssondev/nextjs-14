5bb1f647ec4b0f85945b21aec9b02cb5482187e0
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-27 10:19:32 +0200

    chore: added a routes variable that contains all the non-auth-routes

---
+++ app/lib/routes.ts
@@ -0,0 +1,17 @@
+export const routes = [
+  {
+    id: 1,
+    name: 'Home',
+    link: '/'
+  },
+  {
+    id: 2,
+    name: 'Pricing',
+    link: '/pricing'
+  },
+  {
+    id: 3,
+    name: 'About',
+    link: '/about'
+  }
+]
